44e3b27118cbe831be78566540e89167da69bbbf
Vaibhav Anand <97467803+0xVaibhav11@users.noreply.github.com>
2025-03-07 14:45:26 +0530

    feat: add Darker Grotesque font to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { GeistSans } from "geist/font/sans";
+import { Darker_Grotesque } from "next/font/google";
 import { type Metadata } from "next";
 
 import Footer from "@/components/footer";
@@ -12,11 +13,20 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const darkerGrotesque = Darker_Grotesque({
+  subsets: ["latin"],
+  variable: "--font-darker-grotesque",
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html
+      lang="en"
+      className={`${GeistSans.variable} ${darkerGrotesque.variable}`}
+    >
       <body className="overflow-x-hidden bg-bg-t3 bg-[url('/bg-noise.svg')]">
         <TRPCReactProvider>
           <Navbar />
