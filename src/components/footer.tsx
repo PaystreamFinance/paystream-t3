9d210fa57a0865e822e64e721f597fa8e0b61f46
Vaibhav Anand <97467803+0xVaibhav11@users.noreply.github.com>
2025-03-08 23:16:41 +0530

    feat: redesign footer with new layout, social icons, and enhanced navigation

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,26 +4,98 @@ import Image from "next/image";
 import { Icons } from "./Icons";
 import Logo from "./logo";
 import MaxWidthWrapper from "./max-width-wrapper";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <div className="relative h-[480px] w-full bg-[url('/footer/lines.svg')]">
+    <div className="relative h-[680px] w-full">
+      <div className="pointer-events-none absolute bottom-0 -z-10 h-[600px] w-full select-none">
+        <Image
+          src="/footer/lines.svg"
+          alt="Lines"
+          fill
+          className="object-cover"
+        />
+      </div>
       <MaxWidthWrapper className="flex flex-col items-center">
-        <div className="mt-44 flex w-full items-center justify-between">
-          <Logo />
-
-          <ul className="flex items-center space-x-6 text-sm font-medium leading-[-0.1px] text-[#FFFFFF78]">
-            <li className="cursor-pointer transition-all hover:text-[#9CE0FF]">
-              Optimisers
-            </li>
-            <li className="cursor-pointer transition-all hover:text-[#9CE0FF]">
-              Features
-            </li>
-          </ul>
-
-          <div className="flex items-center gap-6">
-            <Icons.mail className="cursor-pointer" />
-            <Icons.twitter className="cursor-pointer" />
+        <div className="mt-32 grid h-72 w-full grid-cols-3 divide-x divide-border-t3 border border-border-t3 bg-bg-t3">
+          <div className="flex flex-col items-start justify-between p-6">
+            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-[#9FB2C2]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+              >
+                <path
+                  d="M9.33333 1.33334L2.66667 9.33334H8L6.66667 14.6667L13.3333 6.66667H8L9.33333 1.33334Z"
+                  fill="#000D1E"
+                  stroke="#000D1E"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <div className="flex w-full flex-row items-start justify-start gap-7 text-[#BCEBFF99]">
+              <Link href="#">Optimisers</Link>
+              <Link href="#">Features</Link>
+              <Link href="#">Pricing</Link>
+            </div>
+          </div>
+          <div className="flex flex-col items-start justify-between p-6">
+            <div className="flex w-full flex-col items-start justify-between text-[#BCEBFF99]">
+              <Link href="#">What</Link>
+              <Link href="#">How</Link>
+              <Link href="#">Benefits</Link>
+              <Link href="#">Docs</Link>
+            </div>
+            <div className="flex w-full flex-row items-center justify-start gap-6 text-[#BCEBFF99]">
+              <Image
+                src="/social/x-com.svg"
+                alt="Twitter"
+                width={24}
+                height={24}
+                className="cursor-pointer"
+              />
+              <Image
+                src="/social/linkedin.svg"
+                alt="LinkedIn"
+                width={24}
+                height={24}
+                className="cursor-pointer"
+              />
+              <Image
+                src="/social/discord.svg"
+                alt="Discord"
+                width={24}
+                height={24}
+                className="cursor-pointer"
+              />
+              <Image
+                src="/social/telegram.svg"
+                alt="Telegram"
+                width={24}
+                height={24}
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-between p-6">
+            <div className="flex w-full flex-row items-start justify-start gap-7 text-[#BCEBFF99]">
+              <p>Smarter Lending, Easier Borrowing</p>
+            </div>
+            <div className="flex h-14 w-full flex-row items-center justify-between gap-7 border border-border-t3 p-4 text-[#BCEBFF99]">
+              <Image
+                src="/social/telegram.svg"
+                alt="Telegram"
+                width={24}
+                height={24}
+                className="cursor-pointer"
+              />
+              <p>Chat with us</p>
+            </div>
           </div>
         </div>
 
