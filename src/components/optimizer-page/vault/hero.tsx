5f2f1f995e7b7079e98292431b45b26b0ec4fa24
alok8bb <alok8bb@gmail.com>
2025-03-26 19:47:59 +0530

    feat: fetch trader positions to be shown
    Signed-off-by: alok8bb <alok8bb@gmail.com>

--- src/components/optimizer-page/vault/hero.tsx
+++ src/components/optimizer-page/vault/hero.tsx
@@ -1,15 +1,97 @@
-import { VaultGraph } from "./graph";
+"use client";
+
 import VaultActions from "./vault-actions";
+import { VaultGraph } from "./graph";
 
 import StatsGrid, { StatsGridHorizontal } from "./stats-grid";
 import VaultDropdown from "./vault-dropdown";
+import { useEffect, useState } from "react";
+import {
+  useAnchorWallet,
+  useConnection,
+  useWallet,
+} from "@solana/wallet-adapter-react";
+import { AnchorProvider } from "@coral-xyz/anchor";
+import {
+  MarketHeader,
+  MarketHeaderWithPubkey,
+  PaystreamV1Program,
+} from "@meimfhd/paystream-v1";
 
 export interface VaultDataProps {
   vaultTitle: string;
   icon: string;
 }
 
+interface UserData {
+  myPositions: string;
+  apy: string;
+  projectedEarnings: string;
+}
+
 export default function VaultHero({ vaultTitle, icon }: VaultDataProps) {
+  const { publicKey, connected } = useWallet();
+  const wallet = useAnchorWallet();
+
+  const { connection } = useConnection();
+  const provider = new AnchorProvider(connection, wallet!, {});
+  const paystreamProgram = new PaystreamV1Program(provider);
+
+  const [marketHeader, setMarketHeader] =
+    useState<MarketHeaderWithPubkey | null>(null);
+
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    const fetchMarketHeader = async () => {
+      try {
+        const headers = await paystreamProgram.getAllMarketHeaders();
+        if (vaultTitle === "SOL") {
+          // headers[0] is for SOL vault
+          setMarketHeader(headers[1] ?? null);
+        } else if (vaultTitle === "USDC") {
+          // headers[1] is for USDC vault
+          setMarketHeader(headers[0] ?? null);
+        }
+      } catch (error) {
+        console.error("Error fetching market headers:", error);
+      }
+    };
+
+    fetchMarketHeader();
+  }, []);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userData = await paystreamProgram.getTraderPosition(
+          marketHeader?.market!,
+          publicKey!,
+        );
+        const decimals = vaultTitle === "USDC" ? 6 : 9;
+        const apy = vaultTitle === "USDC" ? 7.3637 : 8.4;
+        const onVaultLendsNum =
+          Number(userData?.onVaultLends?.toString() ?? "0") /
+          Math.pow(10, decimals);
+        const inP2pLendsNum =
+          Number(userData?.inP2pLends?.toString() ?? "0") /
+          Math.pow(10, decimals);
+
+        console.log(`Vault Lends ${vaultTitle}:`, onVaultLendsNum.toFixed(2));
+        console.log(`P2P Lends ${vaultTitle}:`, inP2pLendsNum.toFixed(2));
+
+        setUserData({
+          myPositions: onVaultLendsNum.toFixed(2),
+          apy: apy.toString(),
+          projectedEarnings: (onVaultLendsNum * (1 + apy / 100)).toFixed(2),
+        });
+      } catch (error) {
+        console.info("Error fetching user data:", error);
+      }
+    };
+    fetchUserData();
+  }, [marketHeader, connected, publicKey]);
+
   return (
     <main className="relative flex min-h-[1064px] w-full flex-col items-center justify-start border-x border-b border-border-t3">
       <div className="relative flex w-full flex-col gap-4 overflow-hidden px-[46px] pt-[46px]">
