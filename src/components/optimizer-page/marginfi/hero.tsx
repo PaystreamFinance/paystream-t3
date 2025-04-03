1a43e5b3bffc2af790709114a19d704653ab9265
Vaibhav Anand <97467803+0xVaibhav11@users.noreply.github.com>
2025-04-03 18:22:44 +0530

    feat: add Marginfi optimizer pages and components
    - Created MarginfiPage, VaultPage for USDC and SOL, and integrated MaxWidthWrapper and HydrateClient.
    - Added DriftBanner, MarginfiHero, StatsTable, and table-columns for displaying stats and data.
    - Introduced table.css for styling the new tables.

---
+++ src/components/optimizer-page/marginfi/hero.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { AnchorProvider } from "@coral-xyz/anchor";
+import { PaystreamV1Program } from "@meimfhd/paystream-v1";
+import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+import { Loader } from "lucide-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { getDriftStats, getTableData } from "@/lib/data";
+
+import Stats from "../stats";
+import { StatsTable } from "./stats-table";
+import { columns } from "./table-columns";
+
+export default function MarginfiHero() {
+  const [stats, setStats] = useState<
+    { title: string; value: string }[] | undefined
+  >(undefined);
+  const [tableData, setTableData] = useState<any>(undefined);
+  // const tableData = getTableData();
+
+  const wallet = useAnchorWallet();
+  const { connection } = useConnection();
+  const provider = new AnchorProvider(connection, wallet!, {});
+  const paystreamProgram = new PaystreamV1Program(provider);
+
+  useEffect(() => {
+    async function fetchStats() {
+      const stats = await getDriftStats(paystreamProgram);
+      setStats(stats);
+
+      const tableData = await getTableData(paystreamProgram);
+      setTableData(tableData);
+    }
+    fetchStats();
+  }, []);
+
+  return (
+    <main className="relative flex min-h-[1064px] w-full flex-col items-center justify-center border-x border-b border-border-t3">
+      <div className="relative w-full gap-4 overflow-hidden px-3 pt-[30px] sm:px-[46px]">
+        <div className="relative mx-auto h-[300px] w-full max-w-[300px] p-[1px] md:h-[141px] md:w-full md:max-w-none">
+          <div className="absolute inset-0 z-0 bg-[linear-gradient(-45deg,#45A6DE,#0D2039)]" />
+          <div className="relative z-10 flex h-full w-full items-center justify-start bg-bg-t3 bg-no-repeat p-[30px] md:h-[138px]">
+            <div className="absolute inset-0 bg-[linear-gradient(-45deg,#45A6DE,#0D2039)] opacity-20"></div>
+            {/* <div className="pointer-events-none absolute inset-0 select-none">
+              <Image
+                src="/optimizers/marginfi/banner.svg"
+                alt="Marginfi Banner"
+                fill
+                loading="eager"
+                className="object-cover"
+              />
+            </div> */}
+
+            {/* <DriftBanner /> */}
+            {/* Content */}
+            <div className="flex h-full flex-col gap-3">
+              <h3 className="font-darkerGrotesque text-4xl leading-[0.8] text-white">
+                Marginfi Optimizer
+              </h3>
+              <p className="max-w-[341px] font-darkerGrotesque text-[16px] font-normal leading-tight text-white/70">
+                An optimized gateway to Marginfi with the same liquidity and
+                risk parameters.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative w-full px-3 py-[30px] sm:px-[46px]">
+        {!stats ? (
+          <p className="mt-6 flex w-full items-center justify-center gap-2 text-white">
+            <Loader className="size-4 animate-spin text-[#67DFFF]" /> loading
+            stats...
+          </p>
+        ) : (
+          <Stats stats={stats} underline />
+        )}
+      </div>
+
+      <div className="mb-5 flex w-full items-center justify-end px-3 sm:px-[46px]">
+        <Link href="/dashboard">
+          <Button
+            className="group flex items-center gap-2"
+            style={{
+              backgroundColor: "#02142B",
+              color: "#BCEBFF",
+              border: "1px solid #9CE0FF",
+              borderRadius: "8px",
+              padding: "0 24px",
+              fontSize: "12px",
+              height: "34px",
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "8px",
+              whiteSpace: "nowrap",
+              transition: "colors",
+              fontWeight: "normal",
+              cursor: "pointer",
+            }}
+          >
+            Dashboard
+          </Button>
+        </Link>
+      </div>
+
+      <div className="relative min-h-[616px] w-full px-3 pb-[30px] sm:px-[56px]">
+        {!tableData ? (
+          <p className="mt-6 flex w-full items-center justify-center gap-2 text-white">
+            <Loader className="size-4 animate-spin text-[#67DFFF]" /> loading
+            table data...
+          </p>
+        ) : (
+          <StatsTable columns={columns} data={tableData} />
+        )}
+      </div>
+    </main>
+  );
+}
---
+++ src/components/optimizer-page/marginfi/table-columns.tsx
@@ -0,0 +1,161 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, ChevronsUpDown } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export type OptimizerTable = {
+  id: string;
+  asset: "usdc" | "sol";
+
+  balance: number;
+  noOfToken: number;
+  avl_liquidity: number;
+  avl_liquidity_usd: number;
+  borrow_apr: number;
+  supply_apr: number;
+  p2p_apr: number;
+};
+
+export const columns: ColumnDef<OptimizerTable>[] = [
+  {
+    accessorKey: "asset",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Assets
+      </HeaderButton>
+    ),
+    cell: ({ row }) => {
+      return (
+        <Link
+          href={`/optimizers/drift/${row.original.asset}`}
+          className="flex items-center gap-2 hover:opacity-80"
+        >
+          <Image
+            src={`/optimizers/${row.original.asset}.png`}
+            alt={row.original.asset}
+            width={24}
+            height={24}
+            className="rounded-full"
+          />
+          <p className="uppercase text-white">{row.original.asset}</p>
+        </Link>
+      );
+    },
+  },
+  {
+    accessorKey: "balance",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Supply
+      </HeaderButton>
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex flex-col items-start justify-start font-inter">
+          <p className="text-[14px] text-[#FAFAFA]">${row.original.balance}</p>
+          <p className="text-[12px] text-[#9CE0FF66]">
+            {row.original.noOfToken} {row.original.asset}
+          </p>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "avl_liquidity",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Available Liquidity
+      </HeaderButton>
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex flex-col items-start justify-start font-inter">
+          <p className="text-[14px] text-[#FAFAFA]">
+            ${row.original.avl_liquidity_usd}
+          </p>
+          <p className="text-[12px] text-[#9CE0FF66]">
+            {row.original.avl_liquidity} {row.original.asset}
+          </p>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "borrow_apr",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Borrow APR
+      </HeaderButton>
+    ),
+    cell: ({ row }) => {
+      return (
+        <p className="text-[14px] font-light text-[#9CE0FF]">
+          {row.original.borrow_apr}%
+        </p>
+      );
+    },
+  },
+  {
+    accessorKey: "supply_apr",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Supply APR
+      </HeaderButton>
+    ),
+    cell: ({ row }) => {
+      return (
+        <p className="text-[14px] font-light text-[#9CE0FF]">
+          {row.original.supply_apr}%
+        </p>
+      );
+    },
+  },
+  {
+    accessorKey: "p2p_apr",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        P2P APR
+      </HeaderButton>
+    ),
+    cell: ({ row }) => {
+      return (
+        <p className="text-[14px] font-light text-[#9CE0FF]">
+          {row.original.p2p_apr}%
+        </p>
+      );
+    },
+  },
+];
+
+const HeaderButton = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) => {
+  return (
+    <button
+      className="inline-flex items-center gap-1 font-body text-[12px] font-light uppercase text-[#9CE0FF33]"
+      onClick={onClick}
+    >
+      {children}
+      <ChevronsUpDown className="ml-2 h-4 w-4" />
+    </button>
+  );
+};
