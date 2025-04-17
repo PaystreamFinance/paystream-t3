98f8579968ac52da0b942e05abc8a28112c26435
alok8bb <alok8bb@gmail.com>
2025-04-17 17:42:56 +0400

    misc: add new logo
    Signed-off-by: alok8bb <alok8bb@gmail.com>

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,15 +4,14 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <Link href="/">
-      <Image
-        className="select-none rounded-full"
-        src="/logo.svg"
-        width={32}
-        height={32}
-        alt="logo"
-      />
-    </Link>
+    <Image
+      className="h-32 w-32 select-none rounded-full"
+      src="/logo.svg"
+      width={620}
+      height={620}
+      alt="logo"
+      priority
+    />
   );
 };
 
